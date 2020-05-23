0f759871ef8d6f8fdfccb834f62fb97b60c5789f
gsbaker <george@gsbaker.uk>
2020-05-23 23:10:08 +0100

    Finished navbar and applied color scheme.

--- js/publicity-rep-script.js
+++
@@ -1,9 +0,0 @@
-function toggleBody() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-}
-
-function toggleTopBar() {
-    let topBar = document.getElementById('top-bar');
-    topBar.classList.toggle("top-bar-dark");
-}
