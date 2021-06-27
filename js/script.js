1996a6a4bf3712989437e4b1a215829055d7d3dc
gsbaker <gsbaker2000@icloud.com>
2021-06-27 20:59:02 +0100

    updated descriptions and removed JS dark mode

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-let darkModeOn = false;
-if (localStorage.getItem('theme')) {
-    theme = localStorage.getItem('theme');
+let bar = document.getElementById("tabnav-bar");
+let indicator = document.getElementById("tabnav-indicator");
+let defaultToggle = document.getElementById("default-toggle");
 
-    if (theme === "dark") {
-        console.log("theme is dark");
-        toggleDarkMode();
-    }
-}
+let defaultWidth = defaultToggle.getBoundingClientRect().right - defaultToggle.getBoundingClientRect().left;
+indicator.style.width = defaultWidth + "px";
 
-function toggleDarkMode() {
-    console.log("Toggling Dark Mode");
-    document.body.classList.toggle("dark-mode");
-    darkModeOn = !darkModeOn;
+function animateIndicator(toggleElement) {
+    // remove active class from current toggle
+    let activeElements = document.getElementsByClassName("active");
+    let prevToggle = activeElements[0];
+    prevToggle.classList.remove("active");
 
-    let toggleBtn = document.getElementById("dark-mode-toggle");
-    if (darkModeOn) {
-        console.log("Dark Mode On");
-        toggleBtn.classList.remove("btn-light");
-        toggleBtn.classList.add("btn-dark");
-        localStorage.setItem('theme', "dark");
-    } else {
-        console.log("Dark Mode Off");
-        toggleBtn.classList.remove("btn-dark");
-        toggleBtn.classList.add("btn-light");
-        localStorage.setItem('theme', "light");
-    }
+    let currentX = bar.getBoundingClientRect().left;
+    console.log("currentX: " + currentX);
+    let newX = toggleElement.getBoundingClientRect().left;
+    console.log("newX: " + newX);
+    let translateDist = newX - currentX;
+    console.log("translateDist: " + translateDist);
+
+    let newWidth = toggleElement.getBoundingClientRect().right - toggleElement.getBoundingClientRect().left;
+
+    indicator.style.transform = "translateX(" + translateDist + "px)";
+    indicator.style.width = newWidth + "px";
+    toggleElement.classList.add("active");
 }
 
 function toggleDescription(event) {
@@ -46,38 +44,4 @@ var toggles = document.getElementsByClassName("description-toggle");
 for(var k = 0; k < toggles.length; k++) {
     var toggle = toggles[k];
     toggle.addEventListener("click", toggleDescription);
-}
-
-
-var timer = setInterval(nextImage, 6000);
-var curImage = 0;
-var numImages = 6;
-
-function nextImage() {
-    var e;
-    // remove showMe class from current image
-    e = document.getElementById("slide-img-" + curImage);
-    removeClass(e, "showMe");
-
-    // compute next image
-    curImage++;
-    if (curImage > numImages - 1) {
-        curImage = 0;
-    }
-
-    // add showMe class to next image
-    e = document.getElementById("slide-img-" + curImage);
-    addClass(e, "showMe");
-}
-
-function addClass(elem, name) {
-    var c = elem.className;
-    if (c) c += " ";  // if not blank, add a space separator
-    c += name;
-    elem.className = c;
-}
-
-function removeClass(elem, name) {
-    var c = elem.className;
-    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
 }
