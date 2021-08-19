c07becee0d306ac74ba184bfbd7ffd8a7ff24cf2
gsbaker <gsbaker2000@icloud.com>
2021-08-19 16:57:53 +0100

    Updated script to change button color on click

--- pay/script.js
+++ pay/script.js
@@ -1,7 +1,13 @@
 function copyAccountNumber() {
     navigator.clipboard.writeText("36319538");
+    button = document.getElementsByClassName("btn")[0];
+    button.classList.remove("btn-dark");
+    button.classList.add("btn-success");
 }
 
 function copySortCode() {
     navigator.clipboard.writeText("601251");
+    button = document.getElementsByClassName("btn")[1];
+    button.classList.remove("btn-dark");
+    button.classList.add("btn-success");
 }
