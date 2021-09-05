f0d64ebb68c67494001f9ae7dda73df5eceed3bd
gsbaker <gsbaker2000@icloud.com>
2021-09-05 20:39:08 +0100

    Update pay me page

--- pay/script.js
+++ pay/script.js
@@ -1,13 +1,15 @@
 function copyAccountNumber() {
     navigator.clipboard.writeText("36319538");
     button = document.getElementsByClassName("btn")[0];
-    button.classList.remove("btn-dark");
-    button.classList.add("btn-success");
+    button.classList.remove("btn-outline-dark");
+    button.classList.add("btn-outline-success");
+    button.textContent = "Copied ✅";
 }
 
 function copySortCode() {
     navigator.clipboard.writeText("601251");
     button = document.getElementsByClassName("btn")[1];
-    button.classList.remove("btn-dark");
-    button.classList.add("btn-success");
+    button.classList.remove("btn-outline-dark");
+    button.classList.add("btn-outline-success");
+    button.textContent = "Copied ✅";
 }
